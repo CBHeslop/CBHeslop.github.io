dd9721a2c27e4fd97458a0b163ba46ba1b6e2e5f
CBHeslop <hes18001@byui.edu>
2018-11-22 00:35:47 -0700

    updates

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,6 +1,9 @@
 /* get id from html document */
-var t = parseInt(document.getElementById('temp').innerHTML);
-var s = parseInt(document.getElementById('windSpeed').innerHTML);
+var temp = (document.getElementById('temp').innerHTML);
+var windSpeed = (document.getElementById('windSpeed').innerHTML);
+
+var t = parseInt(temp);
+var s = parseInt(windSpeed);
 
 /* formula for wind chill */
 var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16); 
