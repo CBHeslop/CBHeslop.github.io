2f309702a1e48cc8523db9fce7bf1a5f27ae8ae2
CBHeslop <hes18001@byui.edu>
2018-12-12 10:43:32 -0700

    update

--- project_scripts/jsonData.js
+++ project_scripts/jsonData.js
@@ -1,7 +1,7 @@
 var header = document.querySelector('header');
 var section = document.querySelector('section');
 
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var requestURL = 'https://cbheslop.github.io/data/data.json';
 
 var request = new XMLHttpRequest();
 
@@ -15,7 +15,7 @@ request.onload = function() {
 }
 
 function information(data) {
-    var towns = data['temples'];
+    var temples = data['temples'];
 
     for (i = 0; i < temples.length; i++) {
         
@@ -25,26 +25,34 @@ function information(data) {
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
+        var myPara5 = document.createElement('p');
+        var myPara6 = document.createElement('p');
         var myImg = document.createElement('img');
 
-        if(i === 1 || i === 4 || i === 5)
-            myH2.textContent = towns[i].name;
- /*       if(i === 1 || i === 4 || i === 5)
-            myPara1.textContent = towns[i].motto;
-        if(i === 1 || i === 4 || i === 5)
-            myPara2.textContent = 'Year Founded:' + ' ' + towns[i].yearFounded;
-        if(i === 1 || i === 4 || i === 5)
-            myPara3.textContent = 'Population:' + ' ' + towns[i].currentPopulation;
-        if(i === 1 || i === 4 || i === 5)
-            myPara4.textContent = 'Annual Rain Fall:' + ' ' + towns[i].averageRainfall;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myH2.textContent = temples[i].name;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myPara1.textContent = 'Address:' + ' ' + temples[i].address;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myPara2.textContent = 'Telephone:' + ' ' + temples[i].telephone;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myPara3.textContent = 'Email:' + ' ' + temples[i].email;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myPara4.textContent = 'Services:' + ' ' + temples[i].services;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myPara5.textContent = 'History:' + ' ' + temples[i].history;
+        if(i === 0 || i === 1 || i === 2 || i === 3)
+            myPara6.textContent = 'Closure Schedule:' + ' ' + temples[i].closureSchedule;
         
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
         myArticle.appendChild(myPara4);
-        myArticle.appendChild(myImg);
-/*
+        myArticle.appendChild(myPara5);
+        myArticle.appendChild(myPara6);
+/*        myArticle.appendChild(myImg);
+
         if(i === 1) myImg.src="images/12545922.jpg", myImg.alt="Fish Haven Photo";
         if(i === 4) myImg.src="images/2014-10-04_Preston-084.jpg", myImg.alt="Napolean Dynamite Photo";
         if(i === 5) myImg.src="images/SandyMinID_3997.jpg", myImg.alt="Gyser Photo";
