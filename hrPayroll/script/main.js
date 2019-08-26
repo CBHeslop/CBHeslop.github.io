c929fd0ccd753dbe3c26d034c882b3a95c6bebd2
Curtis Heslop <41974332+Heslop-Curtis@users.noreply.github.com>
2019-08-26 17:20:04 -0600

    update websites

--- hrPayroll/script/main.js
+++ hrPayroll/script/main.js
@@ -1,14 +1,39 @@
 
+// make form appear
+$(document).ready(function() {
+    $('#additional-files').click(function() {
+        $('.show-onclick').show();
+    });
+});
+
+
+// menu icon
+function myFunction(x) {
+    x.classList.toggle("change");
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    
+}
+
+
 // Display the results to the screen
 function display() {
     const newOrCurrent = employeeStatus();
     const first = employeeFirstName();
     const last = employeeLastName();
-    let employee = employeeType();
+    const weeklyHours = hourlyCheck();
+    const weeklySales = commissionCheck();
+    let employee = employeeType(first, last, newOrCurrent, weeklyHours, weeklySales);
 
-    let wage = `${first} ${last} is a ${newOrCurrent} ${employee} employee`;
+    let info = `${first} ${last} is a ${newOrCurrent} ${employee} employee`;
+   
 
-    document.getElementById('outputDiv').innerHTML = wage;
+    document.getElementById('outputDiv').innerHTML = info;
+    
 }
 
 
@@ -27,7 +52,7 @@ fs.writeFile('Output.txt', data, (err) => {
 
 // Returns whether the employee is new or current
 function employeeStatus() {
-    const radios = document.getElementsByName('type');
+    const radios = document.getElementsByName('radioType');
 
     for (let i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked == 0) {
@@ -41,19 +66,104 @@ function employeeStatus() {
 }
 
 
-// Returns the employee type from the user selects
-function employeeType() {
-    let salaryType = document.getElementById("salaryType").value;
-    let hourly = document.getElementById("hourly").value;
-    let salary = document.getElementById("salary").value;
-    let commission = document.getElementById("commission").value;  
+// Adds an input if user selects hourly from employee type
+function hourlyCheck() {
+    
+    var elem = document.getElementById('salaryType');
+    if (elem.selectedIndex == 1) {
+        document.getElementById("hourlyDiv").style.display = 'block';
+        var weeklyHours = document.getElementById('hourlyInput').value;
+    } else {
+        document.getElementById("hourlyDiv").style.display = 'none';
+    }
+    return weeklyHours;
+}
+
+
+// Adds an input if user selects commission from employee type
+function commissionCheck() {
+    
+    var elem = document.getElementById('salaryType');
+    if (elem.selectedIndex == 3) {
+        document.getElementById("commissionDiv").style.display = 'block';
+        var weeklySales = document.getElementById('commissionInput').value;
+    } else {
+        document.getElementById("commissionDiv").style.display = 'none';
+    }
+    return weeklySales;
+}
+
+
+// Returns the employee type from the user selects and display to screen
+function employeeType(first, last, newOrCurrent, weeklyHours, weeklySales) {
+    let salaryType = document.getElementById('salaryType').value;
+    let hourly = document.getElementById('hourly').value;
+    let salary = document.getElementById('salary').value;
+    let commission = document.getElementById('commission').value;  
 
     if (salaryType == hourly) {
-        employeeType = "Hourly";
+        if (newOrCurrent == "New") {
+            var hourlyWage = (20.00).toFixed(2);
+        } else {
+            var hourlyWage = (25.00).toFixed(2);
+        }
+        
+        if (weeklyHours > 40) {
+            var overtimeHours = weeklyHours - 40;
+            var overtimeWage = (hourlyWage * 1.5) * overtimeHours;
+        } else {
+            var overtimeHours = 0;
+            var overtimeWage = 0;
+        }
+
+        employeeType = "Hourly"; 
+
+        var hourlyWeeklyTotal = ((hourlyWage * (weeklyHours - overtimeHours)) + overtimeWage).toFixed(2);
+        
+        hourlyEmployee = new Array(weeklyHours, hourlyWage, employeeType);
+
+        let info = `${first} ${last} is a ${newOrCurrent} ${hourlyEmployee[2]} employee`;
+        let wage = `HourlyWage: $${hourlyEmployee[1]} <br> Hours Worked: ${hourlyEmployee[0]}`;
+        let hourlyTotal = `Hourly Weekly Wage: $${hourlyWeeklyTotal}`;
+
+        document.getElementById('outputDiv').innerHTML = info;
+        document.getElementById('outputDiv2').innerHTML = wage;
+        document.getElementById('outputDiv3').innerHTML = hourlyTotal;
+
     } else if (salaryType == salary) {
+        if (newOrCurrent == "New") {
+            var newOrCurrentSalary = (40000).toFixed(2);
+        } else {
+            var newOrCurrentSalary = (60000).toFixed(2);
+        }
+
         employeeType = "Salary";
+
+        var salaryEmployee = (newOrCurrentSalary / 52).toFixed(2);
+
+        let info = `Yearly Salary: $${newOrCurrentSalary}`;
+        let wage = `Salary Weekly Wage: $${salaryEmployee}`;
+
+        document.getElementById('outputDiv2').innerHTML = info;
+        document.getElementById('outputDiv3').innerHTML = wage;
+
     } else if (salaryType == commission) {
+        if (newOrCurrent == "New") {
+            var commissionPercent = (.20 * 100);
+        } else {
+            var commissionPercent = (.30 * 100);
+        }
+
+        var commissionWage = weeklySales * (commissionPercent / 100).toFixed(2);
+
         employeeType = "Commission";
+
+        let info = `Commission Rate: ${commissionPercent}% <br> Weekly Sales: $${weeklySales}`;
+        let wage = `Commission Weekly Wage: $${commissionWage}`;
+
+        document.getElementById('outputDiv2').innerHTML = info;
+        document.getElementById('outputDiv3').innerHTML = wage;
+
     } else {
         document.getElementById('outputDiv').innerHTML = "Please select Employee Type";
         employeeType();
@@ -65,7 +175,7 @@ function employeeType() {
 
 // Returns the employees first name
 function employeeFirstName() {
-    const first = document.getElementById("firstName").value;
+    const first = document.getElementById('firstName').value;
 
     return first;
 }
@@ -73,7 +183,7 @@ function employeeFirstName() {
 
 // Returns the employees last name
 function employeeLastName() {
-    const last = document.getElementById("lastName").value;
+    const last = document.getElementById('lastName').value;
 
     return last;
 }
---
+++ offThePath/script/main.js
@@ -0,0 +1,35 @@
+// Gallery lightbox
+$(function() {
+    const $gallery = $('.gallery a').simpleLightbox();
+})
+
+
+// Bike Gallery 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("bikePhotos");
+ 
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  
+  slides[slideIndex-1].style.display = "block"; 
+}
+
+
+// Trail information
+const apiUrl = 
+
+async function getTrailInfo {
+  const response = await fetch()
+}
