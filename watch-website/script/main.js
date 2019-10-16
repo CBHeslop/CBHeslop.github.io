2fef66ce708340d82f91b41c5b5932a9d79a0b19
Curtis Heslop <41974332+Heslop-Curtis@users.noreply.github.com>
2019-10-16 15:09:32 -0600

    watch images

--- watch-website/script/main.js
+++ watch-website/script/main.js
@@ -17,4 +17,23 @@ async function watches() {
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
+
+    let outputDiv = '';
+
+    for (let i = 0; i < data.watches.length; i++) {
+    
+        outputDiv += `
+        <div class="element">
+            <div class="watchElement">
+                <a onclick="eachWatch()"><img src=""> </a>
+                <h3>${data.watches[i].name}</h3>
+                <h3>${data.watches[i].price}</h3>
+            </div>
+        </div>
+        `;
+    }
+
+    document.getElementById('containerInfo').innerHTML = outputDiv;
+
+
 }
