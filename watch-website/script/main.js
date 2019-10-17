aa9ae0068528edf030a0c8d6630a93f89cb6852b
Curtis Heslop <41974332+Heslop-Curtis@users.noreply.github.com>
2019-10-16 22:29:33 -0600

    id on json data

--- watch-website/script/main.js
+++ watch-website/script/main.js
@@ -25,7 +25,7 @@ async function watches() {
         outputDiv += `
         <div class="element">
             <div class="watchElement">
-                <a onclick="eachWatch()"><img src="${data.watches[i].img}"> </a>
+                <a onclick="eachWatch(${data.watches[i].id})"><img src="${data.watches[i].img}"> </a>
                 <h3>${data.watches[i].name}</h3>
                 <h3>${data.watches[i].price}</h3>
             </div>
@@ -34,6 +34,32 @@ async function watches() {
     }
 
     document.getElementById('containerInfo').innerHTML = outputDiv;
+}
 
+async function eachWatch(id) {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(id);
 
+    let outputDivTwo = '';
+
+    outputDivTwo = `
+    <div id="lightBox">
+        <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+        <div class="movieContainer">
+            <img src="">
+            <div id="watchInformation">
+                <h4>Title: <span class="data">${data.name}</span></h4>
+                <h4>Price: ${data.price}</h4>
+                <h4>Model: ${data.model}</h4>
+                
+            </div>
+        </div>
+    </div>
+    `;
+
+    document.getElementById('containerInfoTwo').innerHTML = outputDivTwo;
+
+    document.getElementById('containerLightBox').style.display='block';
+    document.getElementById('lightBox').style.display='block';
 }
