955233b5134ede4f387877a04a46947adaa54a5b
Curtis Heslop <41974332+Heslop-Curtis@users.noreply.github.com>
2019-10-22 12:17:11 -0600

    update

--- watch-website/script/main.js
+++ watch-website/script/main.js
@@ -60,8 +60,264 @@ misc.onmouseout = function() {
 
 
 
-// watch api
+// watch is connected to a number
 
+function watchesDress() {
+    const watchNumber = 0;
+    watches(watchNumber);
+}
+
+function watchesField() {
+    const watchNumber = 1;
+    watches(watchNumber);
+}
+
+function watchesPilot() {
+    const watchNumber = 2;
+    watches(watchNumber);
+}
+
+function watchesDive() {
+    const watchNumber = 3;
+    watches(watchNumber);
+}
+
+function watchesSport() {
+    const watchNumber = 4;
+    watches(watchNumber);
+}
+
+function watchesMisc() {
+    const watchNumber = 5;
+    watches(watchNumber);
+}
+
+// api for watches
+
+let apiUrl = "https://cbheslop.github.io/watch-website/data/watchData.json";
+
+// api data fetched
+
+async function watches(watchNumber) {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    console.log(watchNumber);
+    let outputDiv = '';
+    
+   // if statement getting json data for correct watch
+    if (watchNumber == 0) {
+        for (let i = 0; i < data.watches.dressWatches.length; i++) {
+            outputDiv += `
+            <div class="element">
+                <div class="watchElement">
+                    <a onclick="eachWatch(${data.watches.dressWatches[i].id}, ${watchNumber})"><img src="${data.watches.dressWatches[i].img}"> </a>
+                    <h2>${data.watches.dressWatches[i].name}</h2>
+                    <h3>${data.watches.dressWatches[i].price}</h3>
+                </div>
+            </div>
+            `;
+        }
+    } else if (watchNumber == 1) {
+        for (let i = 0; i < data.watches.fieldWatches.length; i++) {
+            outputDiv += `
+            <div class="element">
+                <div class="watchElement">
+                    <a onclick="eachWatch(${data.watches.fieldWatches[i].id}, ${watchNumber})"><img src="${data.watches.fieldWatches[i].img}"> </a>
+                    <h2>${data.watches.fieldWatches[i].name}</h2>
+                    <h3>${data.watches.fieldWatches[i].price}</h3>
+                </div>
+            </div>
+            `;
+        }
+    } else if (watchNumber == 2) {
+        for (let i = 0; i < data.watches.pilotWatches.length; i++) {
+            outputDiv += `
+            <div class="element">
+                <div class="watchElement">
+                    <a onclick="eachWatch(${data.watches.pilotWatches[i].id}, ${watchNumber})"><img src="${data.watches.pilotWatches[i].img}"> </a>
+                    <h2>${data.watches.pilotWatches[i].name}</h2>
+                    <h3>${data.watches.pilotWatches[i].price}</h3>
+                </div>
+            </div>
+            `;
+        }
+    } else if (watchNumber == 3) {
+        for (let i = 0; i < data.watches.diveWatches.length; i++) {
+            outputDiv += `
+            <div class="element">
+                <div class="watchElement">
+                    <a onclick="eachWatch(${data.watches.diveWatches[i].id}, ${watchNumber})"><img src="${data.watches.diveWatches[i].img}"> </a>
+                    <h2>${data.watches.diveWatches[i].name}</h2>
+                    <h3>${data.watches.diveWatches[i].price}</h3>
+                </div>
+            </div>
+            `;
+        }
+    } else if (watchNumber == 4) {
+        for (let i = 0; i < data.watches.sportWatches.length; i++) {
+            outputDiv += `
+            <div class="element">
+                <div class="watchElement">
+                    <a onclick="eachWatch(${data.watches.sportWatches[i].id}, ${watchNumber})"><img src="${data.watches.sportWatches[i].img}"> </a>
+                    <h2>${data.watches.sportWatches[i].name}</h2>
+                    <h3>${data.watches.sportWatches[i].price}</h3>
+                </div>
+            </div>
+            `;
+        }
+    } else if (watchNumber == 5) {
+        for (let i = 0; i < data.watches.miscWatches.length; i++) {
+            outputDiv += `
+            <div class="element">
+                <div class="watchElement">
+                    <a onclick="eachWatch(${data.watches.miscWatches[i].id}, ${watchNumber})"><img src="${data.watches.miscWatches[i].img}"> </a>
+                    <h2>${data.watches.miscWatches[i].name}</h2>
+                    <h3>${data.watches.miscWatches[i].price}</h3>
+                </div>
+            </div>
+            `;
+        }
+    }
+
+    document.getElementById('containerInfo').innerHTML = outputDiv;
+
+    smoothScroll();
+}
+
+async function eachWatch(id, watchNumber) {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    
+
+    let outputDivTwo = '';
+
+    if (watchNumber == 0) {
+       
+            outputDivTwo = `
+            <div id="lightBox">
+                <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+                <div class="watchContainer">
+                    <img src="${data.watches.dressWatches[id].img}">
+                    <div id="watchInformation">
+                        <h2><span class="data">${data.watches.dressWatches[id].name}</span></h2>
+                        <h4><span class="size">${data.watches.dressWatches[id].model}</span></h4>
+                        <h3>Price: ${data.watches.dressWatches[id].price}</h3>
+                        <h3>Size: ${data.watches.dressWatches[id].size}</h3>
+                        <h3>Color: ${data.watches.dressWatches[id].color}</h3>
+                        <h3>${data.watches.dressWatches[id].misc}</h3>
+                        <a target="_blank" href="${data.watches.dressWatches[id].url}">Shop Now</a>
+                        
+                    </div>
+                </div>
+            </div>
+            `;
+        
+    } else if (watchNumber == 1) {
+            outputDivTwo = `
+            <div id="lightBox">
+                <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+                <div class="watchContainer">
+                    <img src="${data.watches.fieldWatches[id].img}">
+                    <div id="watchInformation">
+                        <h2><span class="data">${data.watches.fieldWatches[id].name}</span></h2>
+                        <h4><span class="size">${data.watches.fieldWatches[id].model}</span></h4>
+                        <h3>Price: ${data.watches.fieldWatches[id].price}</h3>
+                        <h3>Size: ${data.watches.fieldWatches[id].size}</h3>
+                        <h3>Color: ${data.watches.fieldWatches[id].color}</h3>
+                        <h3>${data.watches.fieldWatches[id].misc}</h3>
+                        <a target="_blank" href="${data.watches.fieldWatches[id].url}">Shop Now</a>
+                        
+                    </div>
+                </div>
+            </div>
+            `;
+    } else if (watchNumber == 2) {
+            outputDivTwo = `
+            <div id="lightBox">
+                <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+                <div class="watchContainer">
+                    <img src="${data.watches.dressWatches[id].img}">
+                    <div id="watchInformation">
+                        <h2><span class="data">${data.watches.dressWatches[id].name}</span></h2>
+                        <h4><span class="size">${data.watches.dressWatches[id].model}</span></h4>
+                        <h3>Price: ${data.watches.dressWatches[id].price}</h3>
+                        <h3>Size: ${data.watches.dressWatches[id].size}</h3>
+                        <h3>Color: ${data.watches.dressWatches[id].color}</h3>
+                        <h3>${data.watches.dressWatches[id].misc}</h3>
+                        <a target="_blank" href="${data.watches.dressWatches[id].url}">Shop Now</a>
+                        
+                    </div>
+                </div>
+            </div>
+            `;
+    } else if (watchNumber == 3) {
+            outputDivTwo = `
+            <div id="lightBox">
+                <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+                <div class="watchContainer">
+                    <img src="${data.watches.dressWatches[id].img}">
+                    <div id="watchInformation">
+                        <h2><span class="data">${data.watches.dressWatches[id].name}</span></h2>
+                        <h4><span class="size">${data.watches.dressWatches[id].model}</span></h4>
+                        <h3>Price: ${data.watches.dressWatches[id].price}</h3>
+                        <h3>Size: ${data.watches.dressWatches[id].size}</h3>
+                        <h3>Color: ${data.watches.dressWatches[id].color}</h3>
+                        <h3>${data.watches.dressWatches[id].misc}</h3>
+                        <a target="_blank" href="${data.watches.dressWatches[id].url}">Shop Now</a>
+                        
+                    </div>
+                </div>
+            </div>
+            `;
+    } else if (watchNumber == 4) {
+            outputDivTwo = `
+            <div id="lightBox">
+                <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+                <div class="watchContainer">
+                    <img src="${data.watches.dressWatches[id].img}">
+                    <div id="watchInformation">
+                        <h2><span class="data">${data.watches.dressWatches[id].name}</span></h2>
+                        <h4><span class="size">${data.watches.dressWatches[id].model}</span></h4>
+                        <h3>Price: ${data.watches.dressWatches[id].price}</h3>
+                        <h3>Size: ${data.watches.dressWatches[id].size}</h3>
+                        <h3>Color: ${data.watches.dressWatches[id].color}</h3>
+                        <h3>${data.watches.dressWatches[id].misc}</h3>
+                        <a target="_blank" href="${data.watches.dressWatches[id].url}">Shop Now</a>
+                        
+                    </div>
+                </div>
+            </div>
+            `;
+    } else if (watchNumber == 5) {
+            outputDivTwo = `
+            <div id="lightBox">
+                <a onclick="document.getElementById('containerLightBox').style.display='none';document.getElementById('lightBox').style.display='none'">Close</a>
+                <div class="watchContainer">
+                    <img src="${data.watches.dressWatches[id].img}">
+                    <div id="watchInformation">
+                        <h2><span class="data">${data.watches.dressWatches[id].name}</span></h2>
+                        <h4><span class="size">${data.watches.dressWatches[id].model}</span></h4>
+                        <h3>Price: ${data.watches.dressWatches[id].price}</h3>
+                        <h3>Size: ${data.watches.dressWatches[id].size}</h3>
+                        <h3>Color: ${data.watches.dressWatches[id].color}</h3>
+                        <h3>${data.watches.dressWatches[id].misc}</h3>
+                        <a target="_blank" href="${data.watches.dressWatches[id].url}">Shop Now</a>
+                        
+                    </div>
+                </div>
+            </div>
+            `;
+    }
+
+   
+    document.getElementById('containerInfoTwo').innerHTML = outputDivTwo;
+
+    document.getElementById('containerLightBox').style.display='block';
+    document.getElementById('lightBox').style.display='block';
+}
+
+/*
 let apiUrl = "https://cbheslop.github.io/watch-website/data/watchData.json";
 
 async function watches() {
@@ -70,7 +326,7 @@ async function watches() {
     console.log(data);
 
     let outputDiv = '';
-
+    
     for (let i = 0; i < data.watches.length; i++) {
     
         outputDiv += `
@@ -83,6 +339,8 @@ async function watches() {
         </div>
         `;
     }
+
+    
 
     document.getElementById('containerInfo').innerHTML = outputDiv;
 
@@ -126,3 +384,4 @@ async function eachWatch(id) {
     document.getElementById('containerLightBox').style.display='block';
     document.getElementById('lightBox').style.display='block';
 }
+*/
