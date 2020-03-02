358e2fe66599497a3bf837fcaeb6999340893d3c
Curtis Heslop <41974332+Heslop-Curtis@users.noreply.github.com>
2020-03-02 06:12:27 -0700

    porfolio update

---
+++ script/main.js
@@ -0,0 +1,10 @@
+// menu icon
+function myFunction(x) {
+    x.classList.toggle("change");
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }    
+}
